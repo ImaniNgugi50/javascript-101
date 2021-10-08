c1f1764a390a51ab0cb1989d8bde23243010c84c
Mo. Ngugi <42456797+ImaniNgugi50@users.noreply.github.com>
2021-10-08 16:39:13 +0300

    Add files via upload

---
+++ JS Variables.js
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html>
+<body>
+
+<h2>Learning about Javascript variables.</h2>
+<p>I will use m,n and O as my variables </p>
+
+<p id="demo"></p>
+
+<script>
+var m= 10;
+var n= 15;
+var O= m+n;
+
+document.getElementById("demo").innerHTML=
+"The value of O is:" + O;
+</script>
+</body>
+</html>
