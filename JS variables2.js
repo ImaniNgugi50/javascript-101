5e802a4c3cd0c2f243c148331a9e9ae0e31c2d93
Mo. Ngugi <42456797+ImaniNgugi50@users.noreply.github.com>
2021-10-08 17:20:15 +0300

    Add files via upload

---
+++ JS variables2.js
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html>
+<body>
+
+<p>In Javascript, numbers are written without quotes while strings are written with quotes </p>
+<p id="demo"></p>
+
+<script>
+var number= 5.87;
+var person= "Maureen Imani";
+var answer= "I am amazing!"
+
+document.getElementById("demo").innerHTML=
+number + "<br>"+person +"<br>"+ answer;
+</script>
+
+</body>
+</html>
