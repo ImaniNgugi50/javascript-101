90c559bb158a99ad44032d1c6634f99815dce6cd
Mo. Ngugi <42456797+ImaniNgugi50@users.noreply.github.com>
2021-10-08 16:44:39 +0300

    Add files via upload

---
+++ JS Undefined.js
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html>
+<body>
+
+<h2> Learning about Javascript Undefined </h2>
+<p id="demo"></p>
+
+<script>
+var carName;
+document.getElementById("demo").innerHTML=carName;
+</script>
+
+</body>
+</html>
