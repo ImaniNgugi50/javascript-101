72bd13639a11d34b536449cf05e9d2de96f90016
Mo. Ngugi <42456797+ImaniNgugi50@users.noreply.github.com>
2021-10-08 17:04:56 +0300

    Add files via upload

---
+++ JS Varnames.js
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html>
+<body>
+
+<h2>My name is...</h2>
+<p id="demo"></p>
+
+<script>
+var m = "Maureen" + " "+ "Imani"+ " " +"Ngugi";
+document.getElementById("demo").innerHTML= m;
+</script>
+
+</body>
+</html>
