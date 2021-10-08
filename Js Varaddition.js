a8845cc7dcecdc780e3e9a5090a99b46dba30aed
Mo. Ngugi <42456797+ImaniNgugi50@users.noreply.github.com>
2021-10-08 16:49:13 +0300

    Add files via upload

---
+++ Js Varaddition.js
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html>
+<body>
+
+<p>How about addition on a variable? </p>
+<p id="demo"></p>
+
+<script>
+var m= 15+2+7;
+document.getElementById("demo").innerHTML = m;
+</script>
+
+</body>
+</html>
